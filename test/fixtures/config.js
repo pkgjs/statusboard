640b846918e597a6987ec604bd381ae0674f0084
Wes Todd <wes@wesleytodd.com>
2019-08-09 13:26:24 -0700

    initial work

---
+++ test/fixtures/config.js
@@ -0,0 +1,41 @@
+'use strict'
+const path = require('path')
+
+module.exports = {
+  db: path.join(__dirname, 'tmp', 'data.db'),
+  outputDirectory: path.join(__dirname, 'tmp'),
+  github: {
+    token: process.env.GH_TOKEN
+  },
+
+  title: 'StatusBoard Test',
+  description: 'A test for StatusBoard',
+
+  orgs: [
+    'pkgjs'
+  ],
+
+  projects: [
+    'expressjs/discussions',
+    {
+      name: 'Express StatusBoard',
+      repo: 'expressjs/statusboard'
+    }
+  ],
+
+  issueLabels: [
+    'discuss',
+    'meeting'
+  ],
+
+  people: [
+    {
+      name: 'Wes Todd',
+      email: 'wes@wesleytodd.com',
+      website: 'http://www.github.com/wesleytodd',
+      npmUsername: 'wesleytodd',
+      githubUsername: 'wesleytodd',
+      twitterUsername: 'wesleytodd'
+    }
+  ]
+}
