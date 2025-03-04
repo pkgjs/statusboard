08ad8745df4cf66fbf95c77086ba625433144775
Sebastian Beltran <bjohansebas@gmail.com>
2025-03-04 11:05:13 -0500

    test: add unit tests for Manifest and Packument classes

--- test/models.test.js
+++ test/models.test.js
@@ -1,45 +1,45 @@
-const assert = require('assert');
-const { Organization } = require('../lib/organization');
+const assert = require('assert')
+const { Organization } = require('../lib/organization')
 const { Project } = require('../lib/project')
-const { describe, it } = require('mocha');
+const { Manifest, Packument } = require('../lib/npm')
+const { describe, it } = require('mocha')
 
-describe('Organization', function() {
-    it('should create an organization with a name string', function() {
-        const org = new Organization('TestOrg');
-        assert.strictEqual(org.name, 'TestOrg');
-        assert.strictEqual(org.displayName, 'TestOrg');
-        assert.strictEqual(org.url, undefined);
-    });
+describe('Organization', function () {
+  it('should create an organization with a name string', function () {
+    const org = new Organization('TestOrg')
+    assert.strictEqual(org.name, 'TestOrg')
+    assert.strictEqual(org.displayName, 'TestOrg')
+    assert.strictEqual(org.url, undefined)
+  })
 
-    it('should create an organization with an object', function() {
-        const org = new Organization({ name: 'TestOrg', displayName: 'Test Organization', url: 'http://test.org' });
-        assert.strictEqual(org.name, 'TestOrg');
-        assert.strictEqual(org.displayName, 'Test Organization');
-        assert.strictEqual(org.url, 'http://test.org');
-    });
+  it('should create an organization with an object', function () {
+    const org = new Organization({ name: 'TestOrg', displayName: 'Test Organization', url: 'http://test.org' })
+    assert.strictEqual(org.name, 'TestOrg')
+    assert.strictEqual(org.displayName, 'Test Organization')
+    assert.strictEqual(org.url, 'http://test.org')
+  })
 
-    it('should use name as displayName if displayName is not provided', function() {
-        const org = new Organization({ name: 'TestOrg' });
-        assert.strictEqual(org.name, 'TestOrg');
-        assert.strictEqual(org.displayName, 'TestOrg');
-        assert.strictEqual(org.url, undefined);
-    });
+  it('should use name as displayName if displayName is not provided', function () {
+    const org = new Organization({ name: 'TestOrg' })
+    assert.strictEqual(org.name, 'TestOrg')
+    assert.strictEqual(org.displayName, 'TestOrg')
+    assert.strictEqual(org.url, undefined)
+  })
 
-    it('should handle empty object input', function() {
-        const org = new Organization({});
-        assert.strictEqual(org.name, undefined);
-        assert.strictEqual(org.displayName, undefined);
-        assert.strictEqual(org.url, undefined);
-    });
+  it('should handle empty object input', function () {
+    const org = new Organization({})
+    assert.strictEqual(org.name, undefined)
+    assert.strictEqual(org.displayName, undefined)
+    assert.strictEqual(org.url, undefined)
+  })
 
-    it('should handle no input', function() {
-        const org = new Organization();
-        assert.strictEqual(org.name, undefined);
-        assert.strictEqual(org.displayName, undefined);
-        assert.strictEqual(org.url, undefined);
-    });
-});
-
+  it('should handle no input', function () {
+    const org = new Organization()
+    assert.strictEqual(org.name, undefined)
+    assert.strictEqual(org.displayName, undefined)
+    assert.strictEqual(org.url, undefined)
+  })
+})
 
 describe('Project', () => {
   it('should initialize with a string repo', () => {
@@ -107,5 +107,80 @@ describe('Project', () => {
     assert.strictEqual(proj.repoBranch, 'main')
     assert.strictEqual(proj.repoDirectory, '/src')
     assert.strictEqual(proj.packageName, null)
+  })
+})
+
+describe('Manifest', function () {
+  it('should correctly initialize with given manifest data', function () {
+    const manifestData = {
+      name: 'test-package',
+      version: '1.0.0',
+      dependencies: { dep1: '^1.0.0' },
+      optionalDependencies: { optDep1: '^1.0.0' },
+      devDependencies: { devDep1: '^1.0.0' },
+      peerDependencies: { peerDep1: '^1.0.0' },
+      bundleDependencies: { bundleDep1: '^1.0.0' },
+      bin: { bin1: 'bin1.js' }
+    }
+
+    const manifest = new Manifest(manifestData)
+
+    assert.strictEqual(manifest.name, manifestData.name)
+    assert.strictEqual(manifest.version, manifestData.version)
+    assert.deepStrictEqual(manifest.dependencies, manifestData.dependencies)
+    assert.deepStrictEqual(manifest.optionalDependencies, manifestData.optionalDependencies)
+    assert.deepStrictEqual(manifest.devDependencies, manifestData.devDependencies)
+    assert.deepStrictEqual(manifest.peerDependencies, manifestData.peerDependencies)
+    assert.deepStrictEqual(manifest.bundleDependencies, manifestData.bundleDependencies)
+    assert.deepStrictEqual(manifest.bin, manifestData.bin)
+  })
+
+  it('should handle missing optional fields', function () {
+    const manifestData = {
+      name: 'test-package',
+      version: '1.0.0'
+    }
+
+    const manifest = new Manifest(manifestData)
+
+    assert.strictEqual(manifest.name, manifestData.name)
+    assert.strictEqual(manifest.version, manifestData.version)
+    assert.strictEqual(manifest.dependencies, undefined)
+    assert.strictEqual(manifest.optionalDependencies, undefined)
+    assert.strictEqual(manifest.devDependencies, undefined)
+    assert.strictEqual(manifest.peerDependencies, undefined)
+    assert.strictEqual(manifest.bundleDependencies, undefined)
+    assert.strictEqual(manifest.bin, undefined)
+  })
+})
+
+describe('Packument', function () {
+  it('should create an instance with the correct properties', function () {
+    const packumentData = {
+      name: 'example-package',
+      distTags: { latest: '1.0.0' },
+      modified: '2023-10-01T00:00:00.000Z',
+      versions: { '1.0.0': {} }
+    }
+
+    const packument = new Packument(packumentData)
+
+    assert.strictEqual(packument.name, 'example-package')
+    assert.deepStrictEqual(packument.distTags, { latest: '1.0.0' })
+    assert.strictEqual(packument.modified, '2023-10-01T00:00:00.000Z')
+    assert.deepStrictEqual(packument.versions, { '1.0.0': {} })
+  })
+
+  it('should handle dist-tags correctly', function () {
+    const packumentData = {
+      name: 'example-package',
+      'dist-tags': { latest: '1.0.0' },
+      modified: '2023-10-01T00:00:00.000Z',
+      versions: { '1.0.0': {} }
+    }
+
+    const packument = new Packument(packumentData)
+
+    assert.deepStrictEqual(packument.distTags, { latest: '1.0.0' })
   })
 })
